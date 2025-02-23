e7c9b33627cd239ae842f13f01c3dcfb482d36be
Youngjae <2015fromjeki@gmail.com>
2025-02-24 00:22:06 +1100

    Server Setting

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
+// External
+import express from "express";
+import dotenv from "dotenv";
 
-console.log("Hello");
+dotenv.config({ path: "/Users/youngjaekim/Desktop/freeStore/.env" });
+const app = express();
+app.use(express.json());
 
-console.log("Hello");
+app.get("/", (req, res) => {
+  return res.status(200).json({ suceess: true, message: "Hello" });
+});
 
+app.listen(5010, () => {
+  console.log("SERVER IS RUUNING");
+});
